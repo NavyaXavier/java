05bf73dc481ca2eb37edf21631552205dabe5f3c
NavyaXavier <74652297+NavyaXavier@users.noreply.github.com>
2021-08-09 21:01:02 +0530

    Update Customer_Bank.java

--- java lab cycle 2/Customer_Bank.java
+++ java lab cycle 2/Customer_Bank.java
@@ -16,19 +16,19 @@ void show()
 System.out.println("Account number of the customer:"+accno);
 System.out.println("Customer Name:"+name);
 System.out.println("Account Type:"+type);
-System.out.println("Amount:" +amount);
+System.out.println("Balance Amount:" +amount);
 }
 void deposit(float a)
 {
-amount=+a;
-System.out.println("deposit amount:"+a);
+amount=amount+a;
+System.out.println("deposit amount:"+amount);
 }
 void withdraw(float a)
 {
 if(amount<a)
 System.out.println("insufficentt balance");
 else
-amount-=a;
+amount=amount-a;
 System.out.println("withdraw amount is:"+a);
 
 }
@@ -41,7 +41,7 @@ class Customer_Bank{
 public static void main(String args[])
 {
 Bank b =new Bank();
-b.details(123456,"Nisha","savings",2000);
+b.details(123456,"anu","savings",2000);
 b.show();
 b.deposit(40000);
 b.checkbalance();
